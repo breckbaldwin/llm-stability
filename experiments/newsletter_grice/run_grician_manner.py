2e87c0728470a66029dc521b029ddcba3a7353f0
Breck Baldwin <breckbaldwin@gmail.com>
2025-07-23 10:52:32 -0400

    initial checkin

---
+++ experiments/newsletter_grice/run_grician_manner.py
@@ -0,0 +1,50 @@
+import os
+import sys
+sys.path.append(os.path.join(os.getcwd()))
+sys.path.append(os.path.join(os.getcwd(), '..', '..'))
+import helper_functions
+import run_experiment
+from datetime import datetime, date, MINYEAR
+import itertools
+
+#MODELS = ['deepseek_R1_0528']
+#MODELS = ['llama3-70b', 'gpt-4o_OAI', 'deepseek_R1_0528']
+
+GRICE_PREFIX = "Please answer the following question while adhering to Gricean Maxims: Particularly the Maxim of Manner: Be clear—avoid obscurity and ambiguity, be brief and orderly. A bit more context, this is a multiple choice question and there is no benefit to the grade in showing your work. You are also being scored on the consistencey of the answer you give over multiple runs so it is suggested you keep your answer to a single letter from A, B, C, D and E and reason conservatively to maximize the chance of giving the same answer across multiple runs of the same question. The question is: "
+
+NO_GRICE = "Please answer the following question, this is a multiple choice question and there is no benefit to the grade in showing your work. You are also being scored on the consistencey of the answer you give over multiple runs so it is suggested you keep your answer to a single letter from A, B, C, D and E and reason conservatively to maximize the chance of giving the same answer across multiple runs of the same question. The question is: "
+
+MODELS = ['gpt-4o_OAI']
+MODEL_CONFIGS = [{'temperature': 0.0, 'seed': 12, 'top_p_k': 0.0,
+                 #'prefix': GRICE_PREFIX}
+                 'prefix': NO_GRICE}
+                 ]
+
+TASKS = ['college_mathematics']
+
+
+TASK_CONFIGS = [{'prompt_type': 'v2', 'shots': 0}]
+
+experiments = itertools.product(MODELS, MODEL_CONFIGS, 
+                                                TASKS, TASK_CONFIGS)
+
+
+for model, model_config, task, task_config in experiments:
+    date = datetime.now()
+    datetime_string = date.strftime("%Y-%m-%d_%H-%M-%S")
+    task_config_in_filename = 'no_grice'
+    
+    run_args = {'output_directory': 'runs/',
+                'model': model,
+                'model_config': model_config,
+                'model_config_in_filename': model_config['temperature'],
+                'task': task,
+                'task_config': task_config,
+                'task_config_in_filename': task_config_in_filename,
+                'num_runs': -1
+#                'limit_num_rubrics': 2
+    }
+#    datetime_string = '0001-01-01_00-00-00'
+    val = run_experiment.run(run_args, datetime_string)
+    if val == "Successfully run":
+        break
---
+++ experiments/newsletter_grice/run_schema.py
@@ -0,0 +1,181 @@
+import os
+import sys
+sys.path.append(os.path.join(os.getcwd()))
+sys.path.append(os.path.join(os.getcwd(), '..', '..'))
+import helper_functions
+import run_experiment
+from datetime import datetime, date, MINYEAR
+
+
+schema = {
+        "type": "json_schema",
+        "json_schema": {
+            "name": "experiment",
+            "schema": {
+                "type": "object",
+                "properties": {
+                                "Answer": {
+                                  "type": "string",
+                                  "enum" : ["Yes", "No"]
+                                }
+                              },
+                              "required": [
+                                "Answer"
+                              ],
+                              "additionalProperties": False
+            },
+            "strict": True
+        }
+}
+
+
+json_schema_Yes_No = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "Answer": {
+      "type": "string",
+      "enum" : ["Yes", "No"]
+    }
+  },
+  "required": [
+    "Answer"
+  ]
+}
+
+
+
+json_schema_prompt_Yes_No = """
+Please answer the following question adhering to these format instructions:
+The output should be formatted as a JSON instance that conforms to the JSON schema below.
+ 
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "Answer": {
+      "type": "string",
+      "enum" : ["Yes", "No"]
+    }
+  },
+  "required": [
+    "Answer"
+  ]
+}
+
+The output {"Answer": "Yes"} is a well-formatted instance of the schema, the output {"Answer": "E"} is not well-formatted. A string answer like "The correct answer is Yes" is not well-formatted.
+
+The question is: 
+
+
+"""
+
+json_schema_prompt_A_D = """
+Please answer the following question adhering to these format instructions:
+The output should be formatted as a JSON instance that conforms to the JSON schema below.
+ 
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "Answer": {
+      "type": "string",
+      "enum" : ["A", "B", "C", "D"]
+    }
+  },
+  "required": [
+    "Answer"
+  ]
+}
+
+The output {"Answer": "A"} is a well-formatted instance of the schema, the output {"Answer": "E"} is not well-formatted. A string answer like "The correct answer is A" is not well-formatted.
+
+The question is: 
+
+
+"""
+
+json_schema_prompt_A_K = """
+Please answer the following question adhering to these format instructions:
+The output should be formatted as a JSON instance that conforms to the JSON schema below.
+ 
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "Answer": {
+      "type": "string",
+      "enum" : ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
+    }
+  },
+  "required": [
+    "Answer"
+  ]
+}
+
+The output {"Answer": "A"} is a well-formatted instance of the schema, the output {"Answer": "L"} is not well-formatted. A string answer like "The correct answer is A" is not well-formatted.
+
+The question is: 
+
+
+"""
+
+
+#MODELS = ['deepseek_R1_0528']
+MODELS = ['gpt-4o_OAI']
+MODEL_CONFIGS = [{'temperature': 0.0, 'seed': 12, 'top_p_k': 0.0,
+                 'prefix': ''}, {'temperature': 0.0, 'seed': 12, 'top_p_k': 0.0}]
+
+YES_NO_TASKS = ['navigate']
+
+A_K_TASKS = ['geometric_shapes']
+
+A_D_TASKS = ['professional_accounting',  'college_mathematics',
+         'logical_deduction', 'ruin_names',        
+          'high_school_european_history',
+         'public_relations'
+        ]
+
+#TASKS = YES_NO_TASKS + A_K_TASKS + A_D_TASKS
+TASKS = ['college_mathematics']
+
+
+TASK_CONFIGS = [{'prompt_type': 'v2', 'shots': 0}]
+                # 'prefix': '\n\n', 'suffix': '\n\n'}]
+                #{'prompt_type': 'v2', 'shots': 0}]
+
+
+
+experiments = helper_functions.experiment_setup(MODELS, MODEL_CONFIGS, 
+                                                TASKS, TASK_CONFIGS)
+
+
+for model, model_config, task, task_config in experiments:
+    date = datetime.now()
+    datetime_string = date.strftime("%Y-%m-%d_%H-%M-%S")
+    task_config_in_filename = 'no-schema'
+    if 'prefix' in model_config:
+        if task in A_D_TASKS:
+            model_config['prefix'] = json_schema_prompt_A_D
+        elif task in YES_NO_TASKS:
+            model_config['prefix'] = json_schema_prompt_Yes_No
+        elif task in A_K_TASKS:
+            model_config['prefix'] = json_schema_prompt_A_K
+        else:
+            print(f"No schema for {task}, skipping")
+            continue
+        task_config_in_filename = 'schema'
+    run_args = {'output_directory': 'runs/',
+                'model': model,
+                'model_config': model_config,
+                'model_config_in_filename': model_config['temperature'],
+                'task': task,
+                'task_config': task_config,
+                'task_config_in_filename': task_config_in_filename,
+                'num_runs': -1,
+#                'limit_num_rubrics': 1
+    }
+#    datetime_string = '0001-01-01_00-00-00'
+    val = run_experiment.run(run_args, datetime_string)
+    if val == "Successfully run":
+        break
